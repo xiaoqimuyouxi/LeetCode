61c3eab5631006ac26d3e21d3e25504481a71f69
ly <805966712@qq.com>
2016-07-12 16:05:28 +0800

    the better method for happy number

--- src/Easy/HappyNumber.java
+++ src/Easy/HappyNumber.java
@@ -41,8 +41,28 @@ public class HappyNumber {
 			return false;
 	}
 	
+	/*
+	 * the size of array is enough for this solution,.if n = 99999999;
+	 * this moment m = 648b < 1000; the value of m decide the index of array
+	 * 2ms
+	 */
+	public static boolean isHappy2(int n){
+		boolean[] exist = new boolean[1000];
+		while(true){
+			if(n == 1)	return true;
+			int sum = 0;
+			while(n>0){
+				sum += (n%10)*(n%10);
+				n /= 10;
+			}
+			n = sum;
+			if(exist[sum] == true)	return false;
+			exist[sum] = true;
+		}
+	}
+	
 	public static void main(String[] args) {
-		System.out.println(isHappy(2));
+		System.out.println(isHappy2(7));
 	}
 
 }
