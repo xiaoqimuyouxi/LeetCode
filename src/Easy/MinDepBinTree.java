3d56f781217c9b1665f93957ee1430582cd5e531
ly <805966712@qq.com>
2016-10-09 16:55:44 +0800

    求二叉树的最小深度

--- src/Easy/MinDepBinTree.java
+++ src/Easy/MinDepBinTree.java
@@ -28,6 +28,18 @@ public class MinDepBinTree {
 		
 	}
 	
+	//1ms
+	public static int minDepth1(TreeNode root) {
+		if(root == null)
+			return 0;
+		int left = minDepth1(root.left);
+		int right = minDepth1(root.right);
+		if(left == 0 || right == 0)
+			return left+right+1;
+		else
+			return 1+Math.min(left, right);
+	}
+	
 	public static void main(String[] args) {
 		TreeNode node1 = new TreeNode(1);
 		TreeNode node2 = new TreeNode(2);
@@ -42,6 +54,6 @@ public class MinDepBinTree {
 		node2.right = node5;
 		node5.right = node6;
 		
-		System.out.println(minDepth(node1));
+		System.out.println(minDepth1(node1));
 	}
 }
