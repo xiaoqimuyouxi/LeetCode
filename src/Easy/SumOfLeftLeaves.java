4c97cfa31de8fb465192cd4c3d89e2b9efd87896
ly <805966712@qq.com>
2016-10-10 22:08:32 +0800

    求二叉树中所有叶子节点中左叶子节点的和

--- src/Easy/SumOfLeftLeaves.java
+++ src/Easy/SumOfLeftLeaves.java
@@ -1,8 +1,14 @@
 package Easy;
 
-
+/**
+ * 404题
+ * 求二叉树所有左叶子结点的和
+ * @author ly
+ *
+ */
 public class SumOfLeftLeaves {
 
+	//9ms
 	public static int sumOfLeftLeaves(TreeNode root) {
 		if(root == null)
 			return 0;
@@ -12,5 +18,22 @@ public class SumOfLeftLeaves {
 		}
 		
 		return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+	}
+	
+	//11ms
+	public static int sumOfLeftLeaves1(TreeNode root) {
+		if(root == null)
+			return 0;
+		int result = 0;
+		if(root.left != null) {
+			if( root.left.left == null && root.left.right == null)
+				result += root.left.val;
+			else
+				result += sumOfLeftLeaves(root.left);
+		}
+		if(root.right != null) 
+			result += sumOfLeftLeaves(root.right);
+		
+		return result;
 	}
 }
