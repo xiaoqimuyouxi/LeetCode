fd5724fcc93d45eded4e957acf26c95cedcec734
ly <805966712@qq.com>
2016-10-19 20:49:27 +0800

    找出数组中第3大的数

--- src/Easy2/ThirdMaximumNumber.java
+++ src/Easy2/ThirdMaximumNumber.java
@@ -32,8 +32,33 @@ public class ThirdMaximumNumber {
         	return max1;
         return max3;
     }
+	
+	//[1,2,-2147483648]此时答案为2，编译出错
+	public static int thirdMax1(int[] nums) {
+		int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE;
+		for(int i = 0; i < nums.length; i++) {
+			if(nums[i]==max1 || nums[i]==max2 || nums[i]==max3)
+				continue;
+			if(max1==Integer.MIN_VALUE || nums[i] > max1) {
+				max3 = max2;
+				max2 = max1;
+				max1 = nums[i];
+			}
+			else if(max2==Integer.MIN_VALUE || nums[i] > max2) {
+				max3 = max2;
+				max2 = nums[i];
+			}
+			else if(max3 == Integer.MIN_VALUE || nums[i] > max3) {
+				max3 = nums[i];
+			}
+		}
+		if(max3 == Integer.MIN_VALUE)
+			return max1;
+		return max3;
+	}
+	
 	public static void main(String[] args) {
-		int[] nums = {2,2,3,1};
-		System.out.println(thirdMax(nums));
+		int[] nums = {1,2,-2147483648};
+		System.out.println(thirdMax1(nums));
 	}
 }
