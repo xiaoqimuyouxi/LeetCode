458b34eba7d11082938ebd17d0dbccbb9f981ffd
ly <805966712@qq.com>
2016-11-14 11:23:38 +0800

    给定excel表的列数获取到该列的列名

---
+++ src/Easy2/ExcelSheetColumnTitle.java
@@ -0,0 +1,34 @@
+package Easy2;
+
+/**
+ * 168题
+ * 给定excel表的列数获取到该列的列名
+ * Created by ly on 2016/11/14.
+ */
+public class ExcelSheetColumnTitle {
+    //0ms
+    public String convertToTitle(int n) {
+        if(n <= 0)
+            return null;
+        StringBuilder res = new StringBuilder();
+        while(n > 0) {
+            char s;
+            //在Z这里经常出问题！！
+            if(n%26 == 0) {
+                s = 'Z';
+                //采用n--可以避免问题发生
+                n--;
+            }
+            else
+                s = (char)(65+(n%26-1));
+            res.append(s);
+            n /= 26;
+        }
+        return res.reverse().toString();
+    }
+    public static void main(String[] args) {
+        ExcelSheetColumnTitle e = new ExcelSheetColumnTitle();
+        String s = e.convertToTitle(702);
+        System.out.println(s);
+    }
+}
