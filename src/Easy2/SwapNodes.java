e11215a1e780b8db18777e41e9000a199a034244
ly <805966712@qq.com>
2016-11-25 19:27:25 +0800

    交换单向链表中的节点对

--- src/Easy2/SwapNodes.java
+++ src/Easy2/SwapNodes.java
@@ -21,4 +21,24 @@ public class SwapNodes {
         }
         return head1;
     }
+
+    //5ms
+    public ListNode swapPairs1(ListNode head) {
+        if(head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = head.next;
+        ListNode a = head, b = a.next, pre = null;
+        while(a != null && b != null) {
+            a.next = b.next;
+            b.next = a;
+            if(pre != null) pre.next = b;
+            if(a.next == null)
+                break;
+            b = a.next.next;
+            pre = a;
+            a = a.next;
+        }
+        return newHead;
+    }
 }
