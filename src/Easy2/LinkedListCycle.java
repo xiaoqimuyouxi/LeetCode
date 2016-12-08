17907148509a6bf9768e38b3111dc0db956e26fb
ly <805966712@qq.com>
2016-12-08 15:46:32 +0800

    添加链表例子

---
+++ src/Easy2/LinkedListCycle.java
@@ -0,0 +1,7 @@
+package Easy2;
+
+/**
+ * Created by ly on 2016/12/8.
+ */
+public class LinkedListCycle {
+}
--- src/Easy2/ListNode.java
+++ src/Easy2/ListNode.java
@@ -6,7 +6,26 @@ package Easy2;
 public class ListNode {
     int val;
     ListNode next;
+
+    public ListNode(){}
     public ListNode(int val) {
         this.val = val;
+    }
+
+    public ListNode test() {
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        ListNode n4 = new ListNode(4);
+        ListNode n5 = new ListNode(2);
+        ListNode n6 = new ListNode(5);
+
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+        n5.next = n6;
+
+        return n1;
     }
 }
