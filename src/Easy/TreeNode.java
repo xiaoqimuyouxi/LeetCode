b2a3b7b8dae9e4f61d2392f97a8206a6930a2975
ly <805966712@qq.com>
2016-12-12 15:19:51 +0800

    添加统一的测试二叉树结构

--- src/Easy/TreeNode.java
+++ src/Easy/TreeNode.java
@@ -13,4 +13,29 @@ public class TreeNode {
 	public TreeNode(int x) {
 		val = x;
 	}
+
+	public TreeNode(){}
+
+	public TreeNode test() {
+		TreeNode n1 = new TreeNode(1);
+		TreeNode n2 = new TreeNode(2);
+		TreeNode n3 = new TreeNode(3);
+		TreeNode n4 = new TreeNode(4);
+		TreeNode n5 = new TreeNode(5);
+		TreeNode n6 = new TreeNode(6);
+		TreeNode n7 = new TreeNode(7);
+		TreeNode n8 = new TreeNode(8);
+		TreeNode n9 = new TreeNode(9);
+
+		n1.left = n2;
+		n1.right = n3;
+		n2.left = n4;
+		n2.right = n5;
+		n3.right = n6;
+		n4.left = n7;
+		n4.right = n8;
+		n6.left = n9;
+
+		return n1;
+	}
 }
