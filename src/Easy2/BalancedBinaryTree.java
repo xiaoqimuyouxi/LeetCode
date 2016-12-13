5e404bf195d0abf2a56c169c6754a4a34295e403
ly <805966712@qq.com>
2016-12-13 15:20:04 +0800

    平衡二叉树

--- src/Easy2/BalancedBinaryTree.java
+++ src/Easy2/BalancedBinaryTree.java
@@ -1,40 +1,39 @@
 package Easy2;
 
 import Easy.TreeNode;
-import sun.reflect.generics.tree.Tree;
 
 import java.util.LinkedList;
 
 /**
- * 110题
- * 平衡二叉树，每个节点的左右子树深度相差不超过1
+ * 110棰�
+ * 骞宠　浜屽弶鏍戯紝姣忎釜鑺傜偣鐨勫乏鍙冲瓙鏍戞繁搴︾浉宸笉瓒呰繃1
  * Created by ly on 2016/12/9.
  */
 public class BalancedBinaryTree {
-    //递归    2ms
+    //閫掑綊    2ms
     public boolean isBalanced(TreeNode root) {
         if(root == null)
             return true;
 
-        //左子树深度与右子树深度相差大于1，就是非平衡二叉树
+        //宸﹀瓙鏍戞繁搴︿笌鍙冲瓙鏍戞繁搴︾浉宸ぇ浜�1锛屽氨鏄潪骞宠　浜屽弶鏍�
         if(Math.abs(getDepth(root.left) - getDepth(root.right)) > 1)
             return false;
         boolean leftBa = isBalanced(root.left);
         boolean rightBa = isBalanced(root.right);
 
-        //递归判断左子树与右子树是否是平衡二叉树
+        //閫掑綊鍒ゆ柇宸﹀瓙鏍戜笌鍙冲瓙鏍戞槸鍚︽槸骞宠　浜屽弶鏍�
         return leftBa && rightBa;
     }
 
-    //二叉树的深度计算，递归解法
+    //浜屽弶鏍戠殑娣卞害璁＄畻锛岄�掑綊瑙ｆ硶
     public int getDepth(TreeNode root) {
         if(root == null)
             return 0;
-        //二叉树左右子树深度的最大值+1才是二叉树的深度
+        //浜屽弶鏍戝乏鍙冲瓙鏍戞繁搴︾殑鏈�澶у��+1鎵嶆槸浜屽弶鏍戠殑娣卞害
         return Math.max(getDepth(root.left), getDepth(root.right))+1;
     }
 
-    //7ms   对于计算二叉树深度迭代法没有递归法快
+    //7ms   瀵逛簬璁＄畻浜屽弶鏍戞繁搴﹁凯浠ｆ硶娌℃湁閫掑綊娉曞揩
     public boolean isBalanced1(TreeNode root) {
         if(root == null)
             return true;
@@ -43,24 +42,24 @@ public class BalancedBinaryTree {
         boolean leftBa = isBalanced(root.left);
         boolean rightBa = isBalanced(root.right);
 
-        //递归判断左子树与右子树是否是平衡二叉树
+        //閫掑綊鍒ゆ柇宸﹀瓙鏍戜笌鍙冲瓙鏍戞槸鍚︽槸骞宠　浜屽弶鏍�
         return leftBa && rightBa;
     }
 
-    //迭代计算二叉树深度
+    //杩唬璁＄畻浜屽弶鏍戞繁搴�
     public int getDepth1(TreeNode root) {
         if(root == null)
             return 0;
         int depth = 0;
-        int currentLevelNodes = 1;  //当前层所包含的节点数
-        int nextLevelNodes = 0;     //下一层所包含的节点数
+        int currentLevelNodes = 1;  //褰撳墠灞傛墍鍖呭惈鐨勮妭鐐规暟
+        int nextLevelNodes = 0;     //涓嬩竴灞傛墍鍖呭惈鐨勮妭鐐规暟
 
         LinkedList<TreeNode> queue = new LinkedList<>();
         queue.add(root);
         while(!queue.isEmpty()) {
-            TreeNode cur = queue.remove();  //从队头移除元素
+            TreeNode cur = queue.remove();  //浠庨槦澶寸Щ闄ゅ厓绱�
             currentLevelNodes--;
-            if(cur.left != null) {  //如果节点有左孩子
+            if(cur.left != null) {  //濡傛灉鑺傜偣鏈夊乏瀛╁瓙
                 queue.add(cur.left);
                 nextLevelNodes++;
             }
@@ -69,7 +68,7 @@ public class BalancedBinaryTree {
                 nextLevelNodes++;
             }
 
-            if(currentLevelNodes == 0) {    //如果该层的所有节点已经遍历完成
+            if(currentLevelNodes == 0) {    //濡傛灉璇ュ眰鐨勬墍鏈夎妭鐐瑰凡缁忛亶鍘嗗畬鎴�
                 depth++;
                 currentLevelNodes = nextLevelNodes;
                 nextLevelNodes = 0;
