6ba5c87f0c30b3fc876be55f2bef27687981a152
ly <805966712@qq.com>
2016-12-19 19:27:08 +0800

    新增生成单链表

--- src/Easy2/ListNode.java
+++ src/Easy2/ListNode.java
@@ -12,20 +12,17 @@ public class ListNode {
         this.val = val;
     }
 
-    public ListNode test() {
-        ListNode n1 = new ListNode(1);
-        ListNode n2 = new ListNode(2);
-        ListNode n3 = new ListNode(3);
-        ListNode n4 = new ListNode(4);
-        ListNode n5 = new ListNode(2);
-        ListNode n6 = new ListNode(5);
-
-        n1.next = n2;
-        n2.next = n3;
-        n3.next = n4;
-        n4.next = n5;
-        n5.next = n6;
-
-        return n1;
+    public static ListNode generateList(int[] arr) {
+        if(arr.length==0)   return null;
+        ListNode head = new ListNode(arr[0]);
+        if(arr.length == 1)
+            return head;
+        ListNode tmp = head;
+        for(int i = 1; i < arr.length; i++) {
+            ListNode nextNode = new ListNode(arr[i]);
+            tmp.next = nextNode;
+            tmp = nextNode;
+        }
+        return head;
     }
 }
---
+++ src/Easy2/PalindromeLinkedList.java
@@ -0,0 +1,7 @@
+package Easy2;
+
+/**
+ * Created by ly on 2016/12/19.
+ */
+public class PalindromeLinkedList {
+}
