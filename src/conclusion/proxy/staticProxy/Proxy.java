1ce81fa607b7815a30ff199133b4b1739b85c1e2
ly <805966712@qq.com>
2017-05-02 09:23:07 +0800

    代理

--- src/conclusion/proxy/staticProxy/Proxy.java
+++ src/conclusion/proxy/staticProxy/Proxy.java
@@ -1,7 +1,9 @@
 package conclusion.proxy.staticProxy;
 
 /**
- * 代理类
+ * 代理类，比如代理商，只会买东西，并不会改变行为，不会制造东西
+ *
+ * 例如通过黄牛买票，黄牛就是一个代理，黄牛可以实现realObject的某些方法，也可以屏蔽一些方法实现，比如改签退票只能去车站
  * Created by ly on 2017/4/10.
  */
 public class Proxy implements Subject {
